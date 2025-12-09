6a86692f96a36adfccdbc7da416c22637ee53997
likemightcap <michaeljlightcap@gmail.com>
2025-12-08 21:51:14 -0500

    Add service worker for PWA install

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
+const CACHE_NAME = "blightsend-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("blightsend-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
