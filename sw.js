9f902a69a16f6082b8380b37bd91c895d87f6a82
likemightcap <michaeljlightcap@gmail.com>
2025-12-18 22:17:08 -0500

    SW: use computed site root for assets; update README references

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
 const CACHE_NAME = "blightsend-v3";
-const ASSETS = [
-  "/blightsend/",
-  "/blightsend/index.html",
-  "/blightsend/style.css",
-  "/blightsend/script.js",
-  "/blightsend/dist/bundle.js",
-  "/blightsend/data/echoes.json",
-  "/blightsend/data/weapons.json",
-  "/blightsend/data/skills.json",
-  "/blightsend/data/armor.json",
-  "/blightsend/data/conditions.json",
-  "/blightsend/manifest.json",
-  "/blightsend/Icons/icon-192.png",
-  "/blightsend/Icons/icon-512.png",
-  "/blightsend/assets/BlightsEnd-Logo.png"
-];
+
+function getSiteRoot() {
+  const path = self.location.pathname || '/';
+  const match = path.match(/^\/(.+?)\//);
+  return match ? `/${match[1]}/` : '/';
+}
+
+const ASSETS = (function () {
+  const root = getSiteRoot();
+  return [
+    root,
+    root + 'index.html',
+    root + 'style.css',
+    root + 'script.js',
+    root + 'dist/bundle.js',
+    root + 'data/echoes.json',
+    root + 'data/weapons.json',
+    root + 'data/skills.json',
+    root + 'data/armor.json',
+    root + 'data/conditions.json',
+    root + 'manifest.json',
+    root + 'Icons/icon-192.png',
+    root + 'Icons/icon-512.png',
+    root + 'assets/BlightsEnd-Logo.png'
+  ];
+})();
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
